6c701be0d9ea3ec0f3b78276e8e5f54bd1eb935c
Belyavtsev Artyem <arsainos@gmail.com>
2019-02-24 18:08:19 +0300

    Add Api class into Compression component

---
+++ CompressionTest/Compression/Utils/API.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CompressionTest.Compression.Data;
+
+namespace CompressionTest.Compression.Utils
+{
+    static class API
+    {
+        public static string[] getCompressionAlgorithms()
+        {
+            return Enum.GetNames(typeof(CompressionAlgorithms));
+        }
+    }
+}
