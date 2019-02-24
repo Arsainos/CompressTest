4c15a4bc2441fcd181c0fcf1d54a997641e4dac8
Belyavtsev Artyem <arsainos@gmail.com>
2019-02-24 14:58:07 +0300

    Add Fabric Method for Compression component, now it much more easier to get class by specific enum :)

---
+++ CompressionTest/Compression/CompressionFabricMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CompressionTest.Compression.Data;
+
+namespace CompressionTest.Compression
+{
+    static class CompressionFabricMethod
+    {
+        public static Compression Create(CompressionAlgorithms algorithmType)
+        {
+            switch (algorithmType)
+            {
+                case CompressionAlgorithms.Gzip:
+                    return new Compression(new Algorithms.GZipCompression());
+
+                case CompressionAlgorithms.Deflate:
+                    return new Compression(new Algorithms.DeflateCompression());
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CompressionTest/Compression/Data/CompressionEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompressionTest.Compression.Data
+{
+    public enum CompressionAlgorithms
+    {
+        Gzip = 0,
+        Deflate = 1
+    }
+}
