63c6346203428cc98dac2be2f6f3411b9fc4b45f
Belyavtsev Artyem <arsainos@gmail.com>
2019-02-24 18:10:09 +0300

    Add api class to IO component

---
+++ CompressionTest/IO/Utils/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompressionTest.IO.Utils
+{
+    static class API
+    {
+        public static string[] GetSourceTypes()
+        {
+            return Enum.GetNames(typeof(IO.Data.DataSource));
+        }
+
+        public static string[] GetDataProvidersTypes()
+        {
+            return Enum.GetNames(typeof(IO.Data.ProviderType));
+        }
+    }
+}
