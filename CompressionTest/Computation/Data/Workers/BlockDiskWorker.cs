adb5de578014220a6fddee8653b62c3b2210d103
Belyavtsev Artyem <arsainos@gmail.com>
2019-02-26 00:53:35 +0300

    Add Provider for workers and also add some mockups for realization classes

---
+++ CompressionTest/Computation/Data/Workers/BlockDiskWorker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompressionTest.Computation.Data.Workers
+{
+    class BlockDiskWorker : Interfaces.IComputation
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CompressionTest/Computation/Workers/WorkerProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompressionTest.Computation.Workers
+{
+    class WorkerProvider : Interfaces.IComputation
+    {
+        public Interfaces.IComputation ComputationStrategy { private get; set; }
+
+        public WorkerProvider(Interfaces.IComputation computation)
+        {
+            ComputationStrategy = computation;
+        }
+
+        public void Start()
+        {
+            ComputationStrategy.Start();
+        }
+    }
+}
